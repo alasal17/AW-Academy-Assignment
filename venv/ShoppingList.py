26dd2196b6bb39281116a8806a8256a570980f23
alasal17 <salam.al.87@hotmail.com>
2020-07-07 11:32:12 +0200

    fixing error

--- venv/ShoppingList.py
+++ venv/ShoppingList.py
@@ -8,7 +8,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-
 # Creating and connect to database
 conn = sqlite3.connect('../asset/database/assiment.db')
 c = conn.cursor()
@@ -17,12 +16,10 @@ c = conn.cursor()
 os.system("cls")
 
 # Create table
-c.execute(
-    'CREATE TABLE IF NOT EXISTS taco_friday(id INTEGER primary key AUTOINCREMENT, item varchar(55) not null unique, price int(55) not null)')
+c.execute('CREATE TABLE IF NOT EXISTS taco_friday(id INTEGER PRIMARY KEY AUTOINCREMENT, item VARCHAR(55) NOT NULL UNIQUE, price INT(55) NOT NULL)')
 conn.commit()
 
 choice = ''
-
 
 
 # Color style
@@ -40,7 +37,8 @@ COLORS = { \
     "cyan-background": "\u001b[46;1m",
 }
 
-# Colors and backgrounds
+
+# Colors and backgrounds function
 def colorText(text):
     for color in COLORS:
         text = text.replace("[[" + color + "]]", COLORS[color])
@@ -52,21 +50,6 @@ f = open("../asset/logo/start_logo.txt")
 ascii = "".join(f.readlines())
 print(colorText(ascii))
 user_budget = float(input('What is your budget? '))
-
-# Options
-program_information = {'For adding a new item': 'add item', 'To see all items': 'all items',
-                       'Creating a new shopping list': 'new list',
-                       'For sorting items from low to high': 'low prices',
-                       'For sorting items from high to low': 'high prices',
-                       'Check for the cheapest item': 'cheapest', 'Check for the expensive item': 'expensive',
-                       'Check for the average price': 'average', 'Update item': 'update',
-                       'Deleting a item by item name': 'delete item', 'Deleting the list': 'delete list',
-                       'When done enter': 'Done'}
-
-# Graph options
-visualization_info = {'Graph for your budget': 'budget', 'Graph for the prices of all items': 'prices'}
-instruation = program_information.items()
-visualization = visualization_info.items()
 
 
 class TacoFriday:
@@ -83,21 +66,11 @@ class TacoFriday:
 
         while done_var != 'done':
 
-            headers = ['', 'OPTIONS']
-            table = tabulate(instruation, headers, tablefmt="github")
+    # List of options
+            self.option_list()
 
-            print("\u001b[33;1m")
-            visualization_table = tabulate(visualization, headers, tablefmt="github")
-
-            print("\u001b[37m")
-            print(table)
-            print("\u001b[33;1m")
-            print(visualization_table)
-            print()
-            print("\u001b[34;1m")
-            print('')
+            print("\u001b[34;1m \n")
             choice = input('What do you want? ').lower()
-            print('')
 
     # Adding new items
             if choice == 'add item':
@@ -123,22 +96,21 @@ class TacoFriday:
     # Find the average price for items
             elif choice == 'average':
                 self.average_price()
-
+    # Delete a item
             elif choice == 'delete item':
                 self.delete_item()
-
+    # Delete the list
             elif choice == 'delete list':
                 self.delete_list()
-
+    # Create a new list
             elif choice == 'new list':
                 self.new_item_list()
-
+    # Graph the item prices
             elif choice == 'prices':
                 self.check_prices_graph()
-
+    # See your budget
             elif choice == 'budget':
                 self.check_budget()
-
     # Close the program
             elif choice == 'done':
                 self.close_program()
@@ -172,65 +144,96 @@ class TacoFriday:
         ascii = "".join(f.readlines())
         print(colorText(ascii))
 
+    def option_list(self):
+
+        # Options
+        program_information = {'For adding a new item': 'add item', 'To see all items': 'all items',
+                               'Creating a new shopping list': 'new list',
+                               'For sorting items from low to high': 'low prices',
+                               'For sorting items from high to low': 'high prices',
+                               'Check for the cheapest item': 'cheapest', 'Check for the expensive item': 'expensive',
+                               'Check for the average price': 'average', 'Update item': 'update',
+                               'Deleting a item by item name': 'delete item', 'Deleting the list': 'delete list',
+                               'When done enter': 'Done'}
+
+        # Graph options
+        visualization_info = {'Graph for your budget': 'budget', 'Graph for the prices of all items': 'prices'}
+        instruction = program_information.items()
+        visualization = visualization_info.items()
+
+        headers = ['', 'OPTIONS']
+        table = tabulate(instruction, headers, tablefmt="github")
+
+        print("\u001b[33;1m")
+        visualization_table = tabulate(visualization, headers, tablefmt="github")
+
+        print("\u001b[37m")
+        print(table)
+        print("\u001b[33;1m")
+        print(visualization_table)
+        print()
+
     def add_item(self):
 
+        c.execute('select sum(price) from taco_friday')
+        sum_of_price = (c.fetchall())
+
+        print(sum_of_price[0])
+
         try:
-            print('')
-            print("\u001b[33;1m")
+            print("\n \u001b[33;1m")
             item = input('Enter item: ')
             price = float(input('Enter price: '))
             c.execute("insert into taco_friday(item, price) values(?, ?)", (item, price))
             conn.commit()
 
             print("\u001b[35m", f"Item: {item} with the price: {price} is added to the shopping list")
-            print("\u001b[34;1m")
-            print('')
+            print("\u001b[34;1m \n")
+
         except sqlite3.Error as e:
-            print('')
-            print("\033[0;31m", e)
+            print("\n \033[0;31m", e)
             print(f"The item {item} is already in the shopping list!")
-            print("\u001b[34;1m")
-            print('')
+            print("\u001b[34;1m \n")
 
     def see_items(self):
 
-        print('')
         c.execute('select * from taco_friday')
         all_items = c.fetchall()
         headers = ['id', 'item', 'price']
 
         if len(all_items) != 0:
-            print('')
-            print("\u001b[36m", tabulate(all_items, headers, tablefmt="github"))
-            print("\u001b[34;1m")
-            print('')
+
+            print("\n \u001b[36m", tabulate(all_items, headers, tablefmt="github"))
+            print("\u001b[34;1m \n")
 
         else:
-            print('')
-            print("\033[0;31m", " EMPTY LIST!")
-            print("\u001b[34;1m")
-            print('')
+            print("\n \033[0;31m", " EMPTY LIST!")
+            print("\u001b[34;1m \n")
 
     def update(self):
         print("\u001b[33;1m")
         update = input('Do you want to update item or price? ').lower()
+
+        c.execute('select * from taco_friday')
         all_items = c.fetchall()
         # Update items
         if len(all_items) !=0:
             if update == 'item':
 
                 try:
-                    choice_item = input('Wish item do you want to update? ')
-                    new_item = input('What is the new item? ')
+                    choice_item = input('What item do you want to update? ')
+                    new_item = input('What is the new item name? ')
                     sql = "update taco_friday set item = ? where item = ? "
                     c.execute(sql, (new_item, choice_item))
                     conn.commit()
 
-                    print('')
-                    print("\u001b[36m", f"The item name {choice_item} was changed to {new_item}")
+                    choice_item_price = input('What is the new price of the item? ')
+                    sql = "update taco_friday set price= ? where item = ?"
+                    c.execute(sql, (choice_item_price, new_item))
+                    conn.commit()
 
-                    print("\u001b[34;1m")
-                    print('')
+                    print("\n \u001b[36m", f"The item name {choice_item} was changed to {new_item} and price is {choice_item_price}")
+                    print("\u001b[34;1m \n")
 
                 except sqlite3.Error as e:
                     print("\033[0;31m", e)
@@ -245,10 +248,10 @@ class TacoFriday:
                     c.execute(sql, (new_price, choice_item))
                     conn.commit()
 
-                    print('')
-                    print("\u001b[36m", f"The item {choice_item} price was changed to {new_price}")
-                    print("\u001b[34;1m")
-                    print(f"")
+
+                    print("\n \u001b[36m", f"The item {choice_item} price was changed to {new_price}")
+                    print("\u001b[34;1m \n")
+
 
                 except sqlite3.Error as e:
 
@@ -256,101 +259,90 @@ class TacoFriday:
                     print("\u001b[34;1m")
 
         else:
-            print('')
-            print("\033[0;31m", " EMPTY LIST!")
-            print("\u001b[34;1m")
-            print('')
+            print("\n \033[0;31m", " EMPTY LIST!")
+            print("\u001b[34;1m \n")
 
     def sort_low_to_high(self):
+
+        c.execute('select * from taco_friday')
         all_items = c.fetchall()
         if len(all_items) != 0:
             headers = ['id', 'item', 'price']
-            print('')
             c.execute('Select * from taco_friday order by price ASC')
             filter_prices = c.fetchall()
 
-            print("\u001b[36m", tabulate(filter_prices, headers, tablefmt="github", stralign='center', numalign='center'))
-            print("\u001b[34;1m")
-            print('')
+            print("\n \u001b[36m", tabulate(filter_prices, headers, tablefmt="github", stralign='center', numalign='center'))
+            print("\u001b[34;1m \n")
+
         else:
-            print('')
-            print("\033[0;31m", " EMPTY LIST!")
-            print("\u001b[34;1m")
-            print('')
+            print("\n \033[0;31m", " EMPTY LIST!")
+            print("\u001b[34;1m \n")
 
     def sort_high_to_low(self):
 
+        c.execute('select * from taco_friday')
         all_items = c.fetchall()
         if len(all_items) != 0:
             headers = ['id', 'item', 'price']
-            print('')
 
             c.execute('Select * from taco_friday order by price DESC')
             filter_prices = c.fetchall()
-            print(f"Filter prices from high to low: ")
+            print(f"\n Filter prices from high to low: ")
             print("\u001b[36m", tabulate(filter_prices, headers, tablefmt="orgtbl"))
-            print("\u001b[34;1m")
-            print('')
+            print("\u001b[34;1m \n")
+
         else:
-            print('')
-            print("\033[0;31m", " EMPTY LIST!")
-            print("\u001b[34;1m")
-            print('')
+            print("\n \033[0;31m", " EMPTY LIST!")
+            print("\u001b[34;1m \n")
 
     def cheapest_item(self):
 
+        c.execute('select * from taco_friday')
         all_items = c.fetchall()
         if len(all_items) != 0:
 
-            print('')
-            c.execute('Select item, min(price) from taco_friday')
-
+            c.execute('Select item, price, min(price) from taco_friday')
             min_price = c.fetchone()
-            print(f"Cheapest item:")
-            print("\033[0;32m", min_price)
-            print("\u001b[34;1m")
-            print('')
+
+            print(f"\n Cheapest item:")
+            print("\033[0;32m", min_price[0], min_price[1], 'kr')
+            print("\u001b[34;1m \n")
+
         else:
-            print('')
-            print("\033[0;31m", " EMPTY LIST!")
-            print("\u001b[34;1m")
-            print('')
+            print("\n \033[0;31m", " EMPTY LIST!")
+            print("\u001b[34;1m \n")
 
     def expensive_item(self):
 
+        c.execute('select * from taco_friday')
         all_items = c.fetchall()
 
         if len(all_items) != 0:
 
-            print('')
-            c.execute('Select item, max(price) from taco_friday')
+            c.execute('Select item, price, max(price) from taco_friday')
             max_price = c.fetchone()
-            print(f"Expensive item:")
-            print("\033[0;31m", max_price)
-            print("\u001b[34;1m")
-            print('')
+            print(f"\n Expensive item:")
+            print("\033[0;31m", max_price[0], max_price[1], 'kr')
+            print("\u001b[34;1m \n")
+
         else:
-            print('')
-            print("\033[0;31m", " EMPTY LIST!")
-            print("\u001b[34;1m")
-            print('')
+            print("\n \033[0;31m", " EMPTY LIST!")
+            print("\u001b[34;1m \n")
 
     def average_price(self):
 
+        c.execute('select * from taco_friday')
         all_items = c.fetchall()
 
         if len(all_items) != 0:
-            print('')
             c.execute('Select avg(price) from taco_friday')
             avg_price = c.fetchone()
-            print("\u001b[36m", f"Average price: {avg_price}")
-            print("\u001b[34;1m")
-            print('')
+
+            print("\n \u001b[36m", f"Average price: {avg_price[0]}")
+            print("\u001b[34;1m \n")
         else:
-            print('')
-            print("\033[0;31m", " EMPTY LIST!")
-            print("\u001b[34;1m")
-            print('')
+            print("\n \033[0;31m", " EMPTY LIST!")
+            print("\u001b[34;1m \n")
 
     def delete_item(self):
 
@@ -365,14 +357,11 @@ class TacoFriday:
             sql = (f"DELETE from taco_friday where item = ?")
             c.execute(sql, (choice_item,))
             conn.commit()
-            print('Item is deleted!')
-            print("\u001b[34;1m")
-            print('')
+            print('\n Item is deleted!')
+            print("\u001b[34;1m \n")
         else:
-            print('Nothing is deleted')
-            print("\u001b[34;1m")
-            print('')
-
+            print('\n Nothing is deleted')
+            print("\u001b[34;1m \n")
 
     def delete_list(self):
 
@@ -393,15 +382,11 @@ class TacoFriday:
             print("\u001b[34;1m")
             print('')
 
-
     def new_item_list(self):
 
-
-        print('')
-        print("\u001b[31;1m", 'Alert! All item list will be deleted')
-        print('')
+        print("\n \u001b[31;1m", 'Alert! All item list will be deleted \n')
         delete_item_list = input('Are you sure you want to create a new item list? (Type Yes or No) ')
-        print("\u001b[34;1m")
+        print("\u001b[34;1m \n")
 
         # Ask if is ok to deleted shopping list
         if delete_item_list == 'yes'.lower():
@@ -410,13 +395,10 @@ class TacoFriday:
                     'CREATE TABLE IF NOT EXISTS taco_friday(id INTEGER primary key AUTOINCREMENT, item varchar(55) not null unique, price int(55) not null)')
             print("\u001b[33;1m", 'Old item list deleted.')
             print("\u001b[33;1m", "New item list created!")
-            print("\u001b[34;1m")
-            print('')
+            print("\u001b[34;1m \n")
         else:
-            print("\u001b[33;1m", 'Continue on the same list!')
-            print("\u001b[34;1m")
-            print('')
-
+            print("\n \u001b[33;1m", 'Continue on the same list!')
+            print("\u001b[34;1m \n")
 
     def check_prices_graph(self):
         query1 = "select * from taco_friday"
@@ -500,7 +482,7 @@ class TacoFriday:
 
     def close_program(self):
 
-        print('The program is done.')
+        print('\u001b[33;1m The program is done.')
 
         COLORS = { \
             "black": "\u001b[30;1m",
@@ -515,12 +497,6 @@ class TacoFriday:
             "black-background": "\u001b[40m",
             "cyan-background": "\u001b[46;1m",
         }
-
-        # You can add more colors and backgrounds to the dictionary if you like.
-        def colorText(text):
-            for color in COLORS:
-                text = text.replace("[[" + color + "]]", COLORS[color])
-            return text
 
         # Example printing out an ASCII file
         f = open("../asset/logo/close_logo.txt")
